fb04399d2fc802f8d93dcd44e1fa0a62d39d3443
Matheus Kyoshi Shoji <63069352+MKShoji@users.noreply.github.com>
2022-11-17 13:50:20 -0300

    Welcome Page Update

--- knowbre/lib/pages/login/welcome_page.dart
+++ knowbre/lib/pages/login/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:knowbre/pages/login/login.dart';
+import 'package:knowbre/pages/upload_post/post_page.dart';
+import 'package:knowbre/shared/themes/app_colors.dart';
 import 'package:knowbre/shared/themes/app_images.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 
@@ -12,89 +16,125 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('assets/images/knowbre_regis.jpg'), 
-        fit: BoxFit.cover,
-        )
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Center(
-                    child: Text(
-                      "Bem-vindo ao KnowBre",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.blue,
-                       fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    )
-                  ),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.only(
-                left: 20,
-                top: 10,
-                right: 16,
-                bottom: 10,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/knowbre_regis.jpg'),
+              colorFilter: ColorFilter.mode(Colors.black12, BlendMode.darken),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black87, Colors.black.withOpacity(0.0)]),
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 70, horizontal: 0),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            text: 'Bem-vindo(a) ao ',
+                            style: TextStyle(fontSize: 32),
+                            children: const <TextSpan>[
+                              TextSpan(
+                                  text: 'Know',
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColor.primary)),
+                              TextSpan(
+                                  text: 'Bre',
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.w100,
+                                      color: AppColor.primary)),
+                            ],
+                          ),
+                        )),
+                  ],
                 ),
-                child: Text(
-                  "Bem-vindo ao KnowBre",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
+              ),
+              Expanded(child: Container()),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 2.5, vertical: 20),
+                        width: 150,
+                        height: 90,
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Get.to(() => AuthPage());
+                          },
+                          elevation: 0,
+                          padding: const EdgeInsets.all(10.0),
+                          color: AppColor.background,
+                          shape: Border.all(color: AppColor.background),
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              color: AppColor.primary,
+                              letterSpacing: 1.5,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        width: 150,
+                        height: 90,
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Get.to(() => RegisterPage());
+                          },
+                          elevation: 0,
+                          padding: const EdgeInsets.all(15.0),
+                          color: Colors.transparent,
+                          shape: Border.all(color: AppColor.background),
+                          child: const Text(
+                            'Cadastra-se',
+                            style: TextStyle(
+                              color: AppColor.background,
+                              letterSpacing: 1.5,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const Spacer(
-                flex: 1,
-              ),
-              Column(
-                children: [
-                  SignInButton(
-                    Buttons.email,
-                    text: "Entrar com email",
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, "/login");
-                    },
-                  ),
-                  const Divider(
-                    height: 15,
-                    thickness: 1.5,
-                    indent: 95,
-                    endIndent: 95,
-                    color: Colors.grey,
-                  ),
-                  SignInButton(
-                    Buttons.googleDark,
-                    text: "Entrar com google",
-                    onPressed: () {},
-                  ),
-                  SignInButton(
-                    Buttons.facebook,
-                    text: "Entrar com facebook",
-                    onPressed: () {},
-                  )
-                ],
-              ),
-              const Spacer(
-                flex: 1,
-              ),
+              )
             ],
           ),
-        ),
+        ],
       ),
     );
   }
